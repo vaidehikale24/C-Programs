572eb38f74a7f3755614aa31722f093a0a0aa0a8
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-07-29 13:28:50 +0530

    Add files via upload

---
+++ Problems on Number/A1.2.c
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////
+//
+//  Display "Marvellous" 5 times
+//
+/////////////////////////////////////////////////
+#include<stdio.h>
+
+void Display(){
+    int i=0;
+
+    for(i=1;i<=5;i++){
+        printf("Marvellous \n");
+    }
+}
+
+int main(){
+
+    Display();
+    return 0;
+}
---
+++ Problems on Number/A1.3.c
@@ -0,0 +1,20 @@
+/////////////////////////////////////////////////
+//
+//  Display numbers from 5 to 1
+//
+/////////////////////////////////////////////////
+#include<stdio.h>
+
+void Display(){
+    int i=0;
+    
+    for(i=5;i>0;i--){
+        printf("%d ",i);
+    }
+}
+
+int main(){
+
+    Display();
+    return 0;
+}
