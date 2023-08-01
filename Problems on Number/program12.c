7b49bd059d809b914d8c208d13be9169dce01b12
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:53:19 +0530

    Update program12.c

--- Problems on Number/program12.c
+++ Problems on Number/program12.c
@@ -1,22 +1,30 @@
-////////////////////////////////////////////////////////////////////////
-//
-//  Accept number from user and display its factors in decreasing order
-//
-////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-void FactRev(int iNo){
-    int iCnt=0;
+//////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   FactRev
+//  Description     :   Accept number from user and display its factors in decreasing order
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+//////////////////////////////////////////////////////////////////////////////////////////////
 
-    for(iCnt=(iNo/2);iCnt>0;iCnt--)
+void FactRev(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = (iNo/2); iCnt > 0; iCnt--)
     {
-        if((iNo%iCnt)==0){
+        if((iNo % iCnt) == 0)
+        {
             printf("%d ",iCnt);
         }
     }
 }
-int main(){
-    int iValue=0;
+int main()
+{
+    int iValue = 0;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
