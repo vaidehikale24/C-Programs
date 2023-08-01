08b9733740ed93761edc0402dedeb5cd66f4c50f
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:51:20 +0530

    Update program6.c

--- Problems on Number/program6.c
+++ Problems on Number/program6.c
@@ -1,21 +1,28 @@
-//////////////////////////////////////////////////////////////
-//
-//  Display "*" for n times using for loop. Accept n from user 
-//
-//////////////////////////////////////////////////////////////
 
 #include<stdio.h>
 
-void Display(int iNo){
-    int iCnt=0;
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Description     :   Display "*" for n times using for loop. Accept n from user 
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+/////////////////////////////////////////////////////////////////////////////////
 
-    for(iCnt=1;iCnt<=iNo;iCnt++){
+void Display(int iNo)
+{
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt <= iNo;iCnt++)
+    {
         printf("*");
     }
 }
 
-int main(){
-    int iValue=0;
+int main()
+{
+    int iValue = 0;
 
     printf("Enter number\n");
     scanf("%d",&iValue);
