057dfe44b292810b5c97f1c798f07f1d8cdcf8fc
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:52:39 +0530

    Update program10.c

--- Problems on Number/program10.c
+++ Problems on Number/program10.c
@@ -1,37 +1,48 @@
-/////////////////////////////////////////////////
-//
-//  Check if the accepted number is Even or Odd
-//
-/////////////////////////////////////////////////
+
 #include<stdio.h>
+
+/////////////////////////////////////////////////
+//
+//  Function Name   :   ChkEven
+//  Description     :   Check if the accepted number is Even or Odd
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+/////////////////////////////////////////////////
 
 #define TRUE 1
 #define FALSE 0
 
 typedef int BOOL;
 
-BOOL ChkEven(int iNo){
-    if(iNo%2==0){
+BOOL ChkEven(int iNo)
+{
+    if(iNo % 2 == 0)
+    {
         return TRUE;
     }
-    else{
+    else
+    {
         return FALSE;
     }
 }
 
-int main(){
-    int iValue=0;
-    BOOL bRet=FALSE;
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
 
-    bRet=ChkEven(iValue);
+    bRet = ChkEven(iValue);
 
-    if(bRet==TRUE){
+    if(bRet == TRUE)
+    {
         printf("%d is Even number",iValue);
     }
-    else{
+    else
+    {
         printf("%d is Odd number",iValue);
     }
 }
