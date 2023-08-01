eb81303643c65be5fd0756d7558f6bb0c658542c
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:50:42 +0530

    Update program4.c

--- Problems on Number/program4.c
+++ Problems on Number/program4.c
@@ -1,37 +1,48 @@
-/////////////////////////////////////////////////
-//
-//  Accept and Check if number is divisible by 5
-//
-/////////////////////////////////////////////////
+
 #include<stdio.h>
+
+/////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Check
+//  Description     :   Accept and Check if number is divisible by 5
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+/////////////////////////////////////////////////////////////////
 
 typedef int BOOL;
 #define TRUE 1
 #define FALSE 0
 
-int Check(int iNo){
+int Check(int iNo)
+{
 
-    if((iNo%5)==0){
+    if((iNo % 5) == 0)
+    {
         return TRUE;
     }
-    else{
+    else
+    {
         return FALSE;
     }
 }
 
-int main(){
-    int iValue=0;
-    BOOL bRet=FALSE;
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
 
-    bRet=Check(iValue);
+    bRet = Check(iValue);
 
-    if(bRet==TRUE){
+    if(bRet == TRUE)
+    {
         printf("Divisible by 5");
     }
-    else{
+    else
+    {
         printf("Not Divisible by 5");
     }
     return 0;
