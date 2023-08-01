3fd0bce0ce0359484603650907a6ffc10586fdca
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:53:58 +0530

    Update program14.c

--- Problems on Number/program14.c
+++ Problems on Number/program14.c
@@ -1,30 +1,39 @@
-////////////////////////////////////////////////////////////////////////
-//
-//  Accept number from user and return summation of all its non factors 
-//
-////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-int SumNonFact(int iNo){
-    int iSum=0;
-    int iCnt=0;
+//////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   SumNonFact
+//  Description     :   Accept number from user and return summation of all its non factors 
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+///////////////////////////////////////////////////////////////////////////////////////////
 
-    for(iCnt=1;iCnt<iNo;iCnt++)
+int SumNonFact(int iNo)
+{
+    int iSum = 0;
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt < iNo; iCnt++)
     {
-        if((iNo%iCnt)!=0){
-            iSum=iSum+iCnt;
+        if((iNo % iCnt) != 0)
+        {
+            iSum = iSum+iCnt;
         }
     }
     return iSum;
 }
-int main(){
-    int iValue=0;
-    int iRet=0;
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
 
-    iRet=SumNonFact(iValue);
+    iRet = SumNonFact(iValue);
     printf("%d",iRet);
+
     return 0;
 }
