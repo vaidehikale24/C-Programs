d12fe0011dc67bc98e5be7d78b25f6915f920a95
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:54:19 +0530

    Update program15.c

--- Problems on Number/program15.c
+++ Problems on Number/program15.c
@@ -1,34 +1,44 @@
-////////////////////////////////////////////////////////////////////////////////////////
-//
-//  Accept number from user and return difference sum of all its factors and non factors
-//
-////////////////////////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-int FactDiff(int iNo){
-    int iSumF=0;
-    int iSumNF=0;
-    int iCnt=0;
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   FactDiff
+//  Description     :   Accept number from user and return difference sum of all its factors and non factors
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-    for(iCnt=1;iCnt<iNo;iCnt++)
+
+int FactDiff(int iNo)
+{
+    int iSumF = 0;
+    int iSumNF = 0;
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt < iNo; iCnt++)
     {
-        if((iNo%iCnt)==0){
-            iSumF=iSumF+iCnt;
+        if((iNo % iCnt) == 0)
+        {
+            iSumF = iSumF+iCnt;
         }
-        else{
-            iSumNF=iSumNF+iCnt;
+        else
+        {
+            iSumNF = iSumNF+iCnt;
         }
     }
     return iSumF-iSumNF;
 }
-int main(){
-    int iValue=0;
-    int iRet=0;
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
 
-    iRet=FactDiff(iValue);
+    iRet = FactDiff(iValue);
     printf("%d",iRet);
     return 0;
 }
