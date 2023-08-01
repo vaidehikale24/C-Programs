91b5968437000da985b64be393e68cb5839493d3
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:52:59 +0530

    Update program11.c

--- Problems on Number/program11.c
+++ Problems on Number/program11.c
@@ -1,31 +1,40 @@
-/////////////////////////////////////////////////////////////////////
-//
-//  Accept number from user and display its multiplication of factors
-//
-/////////////////////////////////////////////////////////////////////
+
 #include<stdio.h>
 
-int MultFact(int iNo){
-    int iResult=1;
-    int iCnt=0;
+/////////////////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   MultFact
+//  Description     :   Accept number from user and display its multiplication of factors
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+//////////////////////////////////////////////////////////////////////////////////////
 
-    for(iCnt=1;iCnt<=(iNo/2);iCnt++)
+int MultFact(int iNo)
+{
+    int iResult = 1;
+    int iCnt = 0;
+
+    for(iCnt = 1;iCnt <= (iNo/2); iCnt++)
     {
-        if((iNo%iCnt)==0){
-            iResult=iResult*iCnt;
+        if((iNo % iCnt) == 0)
+        {
+            iResult = iResult*iCnt;
         }
        
     }
     return iResult;
 }
-int main(){
-    int iValue=0;
-    int iRet=0;
+
+int main()
+{
+    int iValue = 0;
+    int iRet = 0;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
 
-    iRet=MultFact(iValue);
+    iRet = MultFact(iValue);
     printf("Multiplication of factors is : %d ",iRet);
 
     return 0;
