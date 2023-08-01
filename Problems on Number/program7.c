d462643db407bfdf7abcf533cfcf6b76dbbe148d
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:51:37 +0530

    Update program7.c

--- Problems on Number/program7.c
+++ Problems on Number/program7.c
@@ -1,20 +1,28 @@
-/////////////////////////////////////////////////
-//
-//  Display "*" for n times using while. Accept n from user
-//
-/////////////////////////////////////////////////
+
 #include<stdio.h>
 
-void Display(int iNo){
+///////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Description     :   Display "*" for n times using while. Accept n from user
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+//////////////////////////////////////////////////////////////////////////
+
+void Display(int iNo)
+{
     
-    while(iNo>0){
+    while(iNo > 0)
+    {
         printf("*");
         iNo--;
     }
 }
 
-int main(){
-    int iValue=0;
+int main()
+{
+    int iValue = 0;
 
     printf("Enter number\n");
     scanf("%d",&iValue);
