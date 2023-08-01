4f9c5ca823aae5f5eb013d5b3c438d2682f932df
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:52:23 +0530

    Update program9.c

--- Problems on Number/program9.c
+++ Problems on Number/program9.c
@@ -1,20 +1,28 @@
-/////////////////////////////////////////////////
-//
-//  Input   21    3
-//  Output  21   21  21
-//
-/////////////////////////////////////////////////
+
 #include<stdio.h>
 
-void Display(int iNo,int iFrequency){
-    int iCnt=0;
-    for(iCnt=1;iCnt<=iFrequency;iCnt++){
+//////////////////////////////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Input           :   21   3
+//  Output          :   21   21  21
+//  Description     :   Accept two numbers n1 and n2. Print n1 for n2 times.
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//
+//////////////////////////////////////////////////////////////////////////
+
+void Display(int iNo,int iFrequency)
+{
+    int iCnt = 0;
+    for(iCnt = 1;iCnt <= iFrequency; iCnt++)
+    {
         printf("%d ",iNo);
     }
 }
 int main(){
-    int iValue=0;
-    int iCount=0;
+    int iValue = 0;
+    int iCount = 0;
 
     printf("Enter number \n");
     scanf("%d",&iValue);
