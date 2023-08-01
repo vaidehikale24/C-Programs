025b61df9ff0336b3f494033c20989af361f2c5a
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:50:02 +0530

    Update program2.c

--- Problems on Number/program2.c
+++ Problems on Number/program2.c
@@ -1,20 +1,28 @@
-/////////////////////////////////////////////////
-//
-//  Display "Marvellous" 5 times
-//
-/////////////////////////////////////////////////
+
 #include<stdio.h>
 
-void Display(){
-    int i=0;
+//////////////////////////////////////////////////
+//
+//  Function Name   :   Display
+//  Description     :   Display "Marvellous" 5 times
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//  
+//////////////////////////////////////////////////
 
-    for(i=1;i<=5;i++){
+void Display()
+{
+    int i = 0;
+
+    for(i = 1; i <= 5; i++)
+    {
         printf("Marvellous \n");
     }
 }
 
-int main(){
-
+int main()
+{
     Display();
+    
     return 0;
 }
