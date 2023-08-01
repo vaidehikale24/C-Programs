33e38b0e549e4ca95c481cf6b57fbd0eb3676985
vaidehikale24 <99063648+vaidehikale24@users.noreply.github.com>
2023-08-01 14:48:49 +0530

    Update program1.c

--- Problems on Number/program1.c
+++ Problems on Number/program1.c
@@ -1,26 +1,32 @@
-//////////////////////////////////////////////////
-//
-//  Division of two numbers
-//
-//////////////////////////////////////////////////
 
 #include<stdio.h>
 
+//////////////////////////////////////////////////
+//
+//  Function Name   :   Divide
+//  Description     :   Performs division of 2 numbers
+//  Author          :   Vaidehi Vishwanath Kale
+//  Date            :   31/07/2023
+//  
+//////////////////////////////////////////////////
+
 int Divide(int iNo1,int iNo2)
 {
-    int iAns=0;
+    int iAns = 0;
 
-    if(iNo2==0){
+    if(iNo2 == 0)
+    {
         return -1;
     }
-    iAns=iNo1/iNo2;
+    iAns = iNo1/iNo2;
     return iAns;
 }
-int main(){
-    int iValue1=15,iValue2=5;
-    int iRet=0;
+int main()
+{
+    int iValue1 = 15, iValue2 = 5;
+    int iRet = 0;
 
-    iRet=Divide(iValue1,iValue2);
+    iRet = Divide(iValue1,iValue2);
 
     printf("Division is %d",iRet);
 
